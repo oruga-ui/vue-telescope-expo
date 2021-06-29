6b21f31e9811cff255802d76d87c3dfd34ac90da
Andrea Stagi <stagi.andrea@gmail.com>
2021-06-30 01:53:23 +0200

    feat: add ui and framework data

--- src/VueTelescopeExpo.vue
+++ src/VueTelescopeExpo.vue
@@ -1,31 +1,44 @@
 <template>
   <section class="section">
-    <div v-if="loading"><slot v-bind:loading="loading" name="loading">Loading...</slot></div>
     <div class="grid">
       <div class="grid-item"
         v-for="item in items"
         :key="item.id">
         <a
           target="_blank"
+          class="grid-item-link"
           :href="item.url">
           <img
             :src="`https://res.cloudinary.com/nuxt/image/upload/w_${imageWidth},h_${imageWidth * 6 / 8},f_auto,q_auto/${item.screenshotUrl}`"
             class="grid-item-image"
           >
-          <p>
-            <strong>{{ item.hostname }}</strong>
-          </p>
+          <div class="grid-item-details">
+            <p class="grid-item-url">
+              <strong>{{ item.hostname }}</strong>
+            </p>
+            <div class="grid-item-plugins">
+              <img class="grid-item-plugin-image" v-if="item.ui" :src="`https://icons.vuetelescope.com${item.ui.imgPath}`"/>
+              <img class="grid-item-plugin-image" v-if="item.framework" :src="`https://icons.vuetelescope.com${item.framework.imgPath}`"/>
+            </div>
+          </div>
         </a>
       </div>
     </div>
-    <div class="buttons" v-if="items.length && items.length < count">
-      <slot name="buttons" v-bind:loadMore="loadMore"><button @click="loadMore">Load more</button></slot>
-    </div>
-    <div class="buttons" v-if="retry">
-      <slot name="retry" v-bind:setUp="setUp">
-        <div>Something went wrong!</div>
-        <button @click="setUp">Retry</button>
-      </slot>
+    <div class="controls">
+      <div v-if="loading">
+        <slot v-bind:loading="loading" name="loading">{{labelLoading}}</slot>
+      </div>
+      <div v-if="!loading && items.length && items.length < count">
+        <slot name="buttons" v-bind:loadMore="loadMore">
+          <button class="button" @click="loadMore">{{labelLoadMore}}</button>
+        </slot>
+      </div>
+      <div v-if="retry">
+        <slot name="retry" v-bind:setUp="setUp">
+          <div class="retry-label">{{labelError}}</div>
+          <button class="button" @click="setUp">{{labelRetry}}</button>
+        </slot>
+      </div>
     </div>
   </section>
 </template>
@@ -48,6 +61,22 @@ export default {
     sortDirection: {
       type: String,
       default: "desc"
+    },
+    labelRetry: {
+      type: String,
+      default: "Retry"
+    },
+    labelError: {
+      type: String,
+      default: "Something went wrong!"
+    },
+    labelLoadMore: {
+      type: String,
+      default: "Load More"
+    },
+    labelLoading: {
+      type: String,
+      default: "Loading..."
     }
   },
   data() {
@@ -110,6 +139,21 @@ export default {
   box-shadow: 2px 2px 4px #424e5a;
 }
 
+.grid-item-details {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.grid-item-plugin-image {
+  width: 1.5rem;
+  height: 1.5rem;
+}
+
+.grid-item-link {
+  text-decoration:none !important;
+}
+
 @media screen and (min-width: 719px) {
   .grid-item {
     flex: none;
@@ -117,8 +161,26 @@ export default {
   }
 }
 
-.buttons {
+.controls {
   text-align: center;
+}
+
+.button {
+  background-color: #0bdca0;
+  border-color: transparent;
+  color: #fff;
+  cursor: pointer;
+  justify-content: center;
+  padding: calc(.5em - 1px) 1em;
+  text-align: center;
+  white-space: nowrap;
+  border-radius: 4px;
+  padding-left: 1rem;
+  padding-right: 1rem;
+}
+
+.retry-label {
+  padding: 1rem;
 }
 
 .section {
