a9f5df1a25868e6c37db0db9e1bf09001a5b4f9c
Andrea Stagi <stagi.andrea@gmail.com>
2021-07-30 01:53:43 +0200

    feat: add multiple plugins support

--- src/VueTelescopeClient.js
+++ src/VueTelescopeClient.js
@@ -1,9 +1,15 @@
 export default class {
   API_ENDPOINT = "https://api.vuetelescope.com"
-  getItems(slugs, sortField="rank", sortDirection="desc", limit=24, start=0) {
+  getItems(slugs, sortField="rank", sortDirection="asc", limit=12, start=0) {
     const endpoint = new URL(`${this.API_ENDPOINT}/showcases/`)
     for (const property in slugs) {
-      endpoint.searchParams.append(`${property}.slug`, slugs[property]);
+      if (Array.isArray(slugs[property])) {
+        for (const element of slugs[property]) {
+          endpoint.searchParams.append(`${property}.slug`, element);
+        }
+      } else {
+        endpoint.searchParams.append(`${property}.slug`, slugs[property]);
+      }
     }
     endpoint.searchParams.append('_limit', limit)
     endpoint.searchParams.append('_start', start)
@@ -11,10 +17,16 @@ export default class {
     return fetch(endpoint.href)
       .then(response => response.json())
   }
-  getItemsCount(slugs, sortField="rank", sortDirection="desc") {
+  getItemsCount(slugs, sortField="rank", sortDirection="asc") {
     const endpoint = new URL(`${this.API_ENDPOINT}/showcases/count/`)
     for (const property in slugs) {
-      endpoint.searchParams.append(`${property}.slug`, slugs[property]);
+      if (Array.isArray(slugs[property])) {
+        for (const element of slugs[property]) {
+          endpoint.searchParams.append(`${property}.slug`, element);
+        }
+      } else {
+        endpoint.searchParams.append(`${property}.slug`, slugs[property]);
+      }
     }
     endpoint.searchParams.append('_sort', `${sortField}:${sortDirection}`)
     return fetch(endpoint.href)
---
+++ src/VueTelescopeClient.spec.js
@@ -0,0 +1,21 @@
+import VueTelescopeClient from './VueTelescopeClient.js'
+
+
+describe('VueTelescopeClient', () => {
+  it('fetches multiple plugins count', () => {
+    cy.intercept('GET', 'https://api.vuetelescope.com/showcases/count/?framework.slug=nuxtjs&ui.slug=buefy&plugins.slug=vue-i18n&plugins.slug=vue-apollo&_sort=rank%3Aasc', { fixture: 'count_initial.json' })
+    const client = new VueTelescopeClient()
+    return client.getItemsCount({framework: 'nuxtjs', ui: 'buefy', plugins: ['vue-i18n', 'vue-apollo']})
+      .then(response => {
+        expect(response).to.equal(2)
+      })
+  }),
+  it('fetches multiple plugins', () => {
+    cy.intercept('GET', 'https://api.vuetelescope.com/showcases/?framework.slug=nuxtjs&ui.slug=buefy&plugins.slug=vue-i18n&plugins.slug=vue-apollo&_limit=12&_start=0&_sort=rank%3Aasc', { fixture: 'multiple_plugins.json' })
+    const client = new VueTelescopeClient()
+    return client.getItems({framework: 'nuxtjs', ui: 'buefy', plugins: ['vue-i18n', 'vue-apollo']})
+      .then(items => {
+        expect(items[0].domain).to.equal('education.fr')
+      })
+  })
+})
