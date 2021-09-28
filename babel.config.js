58db667ac20afacec028a6f7fc65c84d2251b2ab
Andrea Stagi <stagi.andrea@gmail.com>
2021-09-28 23:52:07 +0200

    chore: NODE_ENV=test for testing

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,18 @@
+const plugins = []
+if (process.env.NODE_ENV === 'test') {
+  plugins.push([
+    "babel-plugin-istanbul", {
+      extension: ['.js', '.vue'],
+    }
+  ])
+}
+
+
 module.exports = {
   presets: [
     ['@vue/app', {
       useBuiltIns: false
     }]
   ],
-  plugins: [
-    ['babel-plugin-istanbul', {
-      extension: ['.js', '.vue']
-    }]
-  ]
+  plugins
 }
